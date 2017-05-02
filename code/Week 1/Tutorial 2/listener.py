a6974ba44db88fd80553272277edd9a735effa39
Madhur Behl <behl.madhur@gmail.com>
2017-05-02 16:07:29 -0400

    Update listener.py

--- code/Week 1/Tutorial 2/listener.py
+++ code/Week 1/Tutorial 2/listener.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 
+# 
 import rospy
 from std_msgs.msg import String
 
-def callback(data):
+def callback(data): # whenever a message arrives on the chatter topic, it is logged. 
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
     
 def listener():
 
-    rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber("chatter", String, callback)
+    rospy.init_node('listener', anonymous=True) # Node called Listner
+    rospy.Subscriber("chatter", String, callback) # Node subscribes to the topic chatter (mesage is of type string)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
