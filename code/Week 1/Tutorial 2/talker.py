cfb5b7ee3211d5bda1d47e8b609f9efffdaa1cb3
Madhur Behl <behl.madhur@gmail.com>
2017-05-02 16:12:11 -0400

    Update talker.py

--- code/Week 1/Tutorial 2/talker.py
+++ code/Week 1/Tutorial 2/talker.py
@@ -4,16 +4,16 @@ import rospy
 from std_msgs.msg import String
 
 def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
+    pub = rospy.Publisher('chatter', String, queue_size=10) # Creating a publisher called pub, publishes to topic chatter, message type String.
+    rospy.init_node('talker', anonymous=True) # Create a node called talker.
+    rate = rospy.Rate(10) # 10hz the rate at which the next loop is run
+    while not rospy.is_shutdown(): 
         hello_str = "hello world %s" % rospy.get_time()
-        pub.publish(hello_str)
+        pub.publish(hello_str)  # the hellow world message of type string is published to chatter, by node talker.
         rate.sleep()
 
 if __name__ == '__main__':
     try:
-        talker()
-    except rospy.ROSInterruptException:
+        talker()    
+    except rospy.ROSInterruptException: # exit gracefully.
         pass
